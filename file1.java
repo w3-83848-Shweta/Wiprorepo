204ed0c66340963ac006d660da3db7a9adddbc4e
Shweta_Jamdade <shwetajamdade4@gmail.com>
2024-05-16 17:08:35 +0530

    data deleted

--- file1.java
+++ file1.java
@@ -2,6 +2,8 @@ import java.lang.*;
 
 public class first{
  int a=500;
+ int b=60
+
 public static void main(String [] args){
  System.out.println("This is first java prg");
  System.out.println("This is second line");
