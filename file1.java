e1ba65bc2a029c46b36bf2c26be88c4a987a95d6
Shweta_Jamdade <162580507+w3-83848-Shweta@users.noreply.github.com>
2024-05-16 14:38:33 +0530

    Update file1.java
    Integer value added

--- file1.java
+++ file1.java
@@ -1,6 +1,7 @@
 import java.lang.*;
 
 public class first{
+ int a=500;
 public static void main(String [] args){
  System.out.println("This is first java prg");
  System.out.println("This is second line");
